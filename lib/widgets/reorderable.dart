b8282645ed3236f0e793e9fd2796a25d14613af7
Boerger <andre.boerger@lyth.de>
2021-10-19 21:30:39 +0200

    fix

--- lib/widgets/reorderable.dart
+++ lib/widgets/reorderable.dart
@@ -221,66 +221,66 @@ class _ReorderableState extends State<Reorderable> with WidgetsBindingObserver {
         enabled: !widget.lockedChildren.contains(index),
       ),
     );
-    return SingleChildScrollView(
-      child: Builder(
-        builder: (context) {
-          // after all children are added to animatedChildren
-          if (hasBuiltItems && childrenCopy.length == _childrenIdMap.length) {
-            return SingleChildScrollView(
-              key: _copyReorderableKey,
-              physics: widget.physics,
-              child: SizedBox(
-                height: _wrapSize.height,
-                width: _wrapSize.width,
-                child: Stack(
-                  children: _childrenIdMap.entries
-                      .map((e) => AnimatedDraggableItem(
-                            key: Key(e.key.toString()),
-                            enableAnimation: widget.enableAnimation,
-                            entry: e,
-                            enableLongPress: widget.enableLongPress,
-                            onDragUpdate: _handleDragUpdate,
-                            longPressDelay: widget.longPressDelay,
-                            enabled: !widget.lockedChildren.contains(e.key),
-                            child: childrenCopy[e.value.orderId],
-                          ))
-                      .toList(),
-                ),
+    return Builder(
+      builder: (context) {
+        // after all children are added to animatedChildren
+        if (hasBuiltItems && childrenCopy.length == _childrenIdMap.length) {
+          return SingleChildScrollView(
+            key: _copyReorderableKey,
+            physics: widget.physics,
+            child: SizedBox(
+              height: _wrapSize.height,
+              width: _wrapSize.width,
+              child: Stack(
+                children: _childrenIdMap.entries
+                    .map((e) => AnimatedDraggableItem(
+                          key: Key(e.key.toString()),
+                          enableAnimation: widget.enableAnimation,
+                          entry: e,
+                          enableLongPress: widget.enableLongPress,
+                          onDragUpdate: _handleDragUpdate,
+                          longPressDelay: widget.longPressDelay,
+                          enabled: !widget.lockedChildren.contains(e.key),
+                          child: childrenCopy[e.value.orderId],
+                        ))
+                    .toList(),
               ),
-            );
-          } else {
-            switch (widget.reorderableType) {
-              case ReorderableType.wrap:
-                return Wrap(
+            ),
+          );
+        } else {
+          switch (widget.reorderableType) {
+            case ReorderableType.wrap:
+              return SingleChildScrollView(
+                child: Wrap(
                   key: _wrapKey,
                   spacing: widget.spacing,
                   runSpacing: widget.runSpacing,
                   children: generatedChildren,
-                );
-              case ReorderableType.gridView:
-                throw UnimplementedError('Widget soon available!');
-              case ReorderableType.gridViewCount:
-                return GridView.count(
-                  key: _wrapKey,
-                  crossAxisCount: widget.crossAxisCount!,
-                  mainAxisSpacing: widget.mainAxisSpacing,
-                  children: generatedChildren,
-                );
-              case ReorderableType.gridViewExtent:
-                return GridView.extent(
-                  key: _wrapKey,
-                  shrinkWrap: widget.shrinkWrap,
-                  physics: widget.physics,
-                  maxCrossAxisExtent: widget.maxCrossAxisExtent,
-                  clipBehavior: widget.clipBehavior,
-                  mainAxisSpacing: widget.mainAxisSpacing,
-                  crossAxisSpacing: widget.crossAxisSpacing,
-                  children: generatedChildren,
-                );
-            }
+                ),
+              );
+            case ReorderableType.gridView:
+              throw UnimplementedError('Widget soon available!');
+            case ReorderableType.gridViewCount:
+              return GridView.count(
+                key: _wrapKey,
+                crossAxisCount: widget.crossAxisCount!,
+                mainAxisSpacing: widget.mainAxisSpacing,
+                children: generatedChildren,
+              );
+            case ReorderableType.gridViewExtent:
+              return GridView.extent(
+                key: _wrapKey,
+                shrinkWrap: widget.shrinkWrap,
+                physics: widget.physics,
+                maxCrossAxisExtent: widget.maxCrossAxisExtent,
+                clipBehavior: widget.clipBehavior,
+                mainAxisSpacing: widget.mainAxisSpacing,
+                crossAxisSpacing: widget.crossAxisSpacing,
+                children: generatedChildren,
+              );
           }
-        },
-      ),
+        }
+      },
     );
   }
 
